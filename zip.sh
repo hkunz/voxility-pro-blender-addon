8e023c50ddf1f4ff37d15c7e5e5a739cc577b9d8
Harry Kunz <harry.kunz@ymail.com>
2024-01-14 22:01:03 +0800

    zip exclude more files

--- zip.sh
+++ zip.sh
@@ -8,4 +8,6 @@ rm -f "$parent_folder.zip"
 zip -r "$parent_folder.zip" "$parent_folder"/* \
   --exclude "$parent_folder/.vscode/*" \
   --exclude "$parent_folder/.git/*" \
+  --exclude "$parent_folder/temp/*" \
+  --exclude "$parent_folder/__pycache__/*" \
   --exclude "$parent_folder/$(basename "$0")"
