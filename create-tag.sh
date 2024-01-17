6f71121207384ec3f2a0c7ab266c18b5406b8002
Harry Kunz <harry.kunz@ymail.com>
2024-01-17 13:42:24 +0800

    more checks for create-tag.sh

--- create-tag.sh
+++ create-tag.sh
@@ -1,29 +1,61 @@
 #!/bin/bash
 
-# Check if the correct number of arguments is provided
-if [ "$#" -ne 1 ]; then
-    echo "Usage: $0 <version>"
-    exit 1
+num_args="$#"
+version="$1"
+
+# Parse command-line options
+while [[ $# -gt 0 ]]; do
+    case $1 in
+        --increment|-i)
+            increment=true
+            shift
+            ;;
+        *)
+            shift
+            ;;
+    esac
+done
+
+# If increment is specified, increment the patch version
+if [ "$increment" = true ]; then
+    latest_tag=$(git tag -l --sort=-v:refname | head -n 1 | sed 's/^v//')
+    if [ -z "$latest_tag" ]; then
+        version="0.0.1"
+        tag_name="v$version"
+        echo "No tags yet. Starting with version $tag_name"
+    else
+        echo "Latest tag version: $latest_tag"
+        IFS='.' read -r v_major v_minor v_patch <<< "$latest_tag"
+        ((v_patch++))
+        version="$v_major.$v_minor.$v_patch"
+        tag_name="v$version"
+        echo "New tag version: $tag_name"
+    fi
+else
+    # Check if the correct number of arguments is provided
+    if [ "$num_args" -ne 1 ]; then
+        echo "Usage: $0 <version>"
+        exit 1
+    fi
+
+    # Validate the version format (X.X.X)
+    if ! [[ $version =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        echo "Invalid version format. Please use X.X.X (e.g., 1.0.0)"
+        exit 1
+    fi
+
+    IFS='.' read -r v_major v_minor v_patch <<< "$version"
+    tag_name="v$version"
+
+    # Check if the version tag already exists
+    if git rev-parse -q --verify "v$version" >/dev/null; then
+        echo "Tag v$version already exists in Git. Aborting."
+        exit 1
+    fi
 fi
-
-# Validate the version format (X.X.X)
-if ! [[ $1 =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    echo "Invalid version format. Please use X.X.X (e.g., 1.0.0)"
-    exit 1
-fi
-
-# Check if the version tag already exists
-if git rev-parse -q --verify "v$1" >/dev/null; then
-    echo "Tag v$1 already exists in Git. Aborting."
-    exit 1
-fi
-
-
-IFS='.' read -r v_major v_minor v_patch <<< "$1"
-
-tag_name="v$1"
 
 echo "Attempting to update __init__.py to reflect the new version."
+exit
 
 read -p "Do you want to commit this change? [y/n]: " answer
 
@@ -39,11 +71,11 @@ fi
 
 sed -i "s/\"version\": ([0-9]\+, [0-9]\+, [0-9]\+)/\"version\": ($v_major, $v_minor, $v_patch)/g" __init__.py
 git add __init__.py
-git commit -m "Update version in __init__.py to $1"
+git commit -m "Update version in __init__.py to $version"
 git push
 echo "Version change to __init__.py committed successfully."
 
-git tag -a "$tag_name" -m "Release version $1"
+git tag -a "$tag_name" -m "Release version $version"
 git push origin "$tag_name"
 
 echo "Tag $tag_name created and pushed successfully."
