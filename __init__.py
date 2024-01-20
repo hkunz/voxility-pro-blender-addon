ce26bf03bfba59c8f9ade4de12d62610924465d8
Harry Kunz <harry.kunz@ymail.com>
2024-01-20 20:11:22 +0800

    use GPL license

--- __init__.py
+++ __init__.py
@@ -6,13 +6,18 @@
 # MagicaVoxel Vox Exporter
 # Copyright (c) 2024 Harry McKenzie
 #
-# This program is licensed to you under a Royalty-Free License.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# You are allowed to use, modify, and distribute this software,
-# royalty-free, for both personal and commercial purposes.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 # ##### END LICENSE BLOCK #####
 
