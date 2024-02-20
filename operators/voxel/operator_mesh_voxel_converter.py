1ad3e7b4c58aedcf81a20580b3e331729f2aa4f0
Harry Kunz <harry.kunz@ymail.com>
2024-02-20 20:29:13 +0800

    create a subdir for the target output

--- operators/voxel/operator_mesh_voxel_converter.py
+++ operators/voxel/operator_mesh_voxel_converter.py
@@ -21,6 +21,7 @@ class OBJECT_OT_MeshVoxelConvertOperator(OperatorVoxconvert):
     bl_label = "Voxility Pro Mesh-Voxel Convert "
     bl_description = "Voxelize or convert selected objects into a single voxel object"
 
+    TEMP_DIR: str = None
     TARGET_FORMAT: str = None
 
     vox_target_format_ext: bpy.props.StringProperty(
@@ -67,40 +68,48 @@ class OBJECT_OT_MeshVoxelConvertOperator(OperatorVoxconvert):
         c.vc_merge_vertices = int(properties.merge_vertices)
         return c
 
+    def generate_output_paths(self, temp_dir: str) -> List[str]:
+        output_paths = [os.path.join(temp_dir, 'temp_out.obj')]
+        type: str = self.vox_target_format_ext.lower()
+        if type != "none":
+            type_dir = os.path.join(temp_dir, type)
+            os.makedirs(type_dir, exist_ok=True)
+            output_paths.append(os.path.join(type_dir, f'temp_out.{type}'))
+        return output_paths
+
     def execute(self, context: bpy_types.Context) -> set[str]:
         voxelize_duration: float = time.time()
         active_object: bpy_types.Object = context.view_layer.objects.active
-        temp_dir: str = TempFileManager().create_temp_dir()
+        if OBJECT_OT_MeshVoxelConvertOperator.TEMP_DIR is None:
+            OBJECT_OT_MeshVoxelConvertOperator.TEMP_DIR = TempFileManager().create_temp_dir()
+        temp_dir: str = OBJECT_OT_MeshVoxelConvertOperator.TEMP_DIR
         vc_in_path: str = os.path.join(temp_dir, 'temp_in.obj')
-        vc_out_path: str = os.path.join(temp_dir, 'temp_out.obj')
         objects: List[bpy_types.Object] = context.selected_objects.copy()
         self.create_temp_dup(objects)
         self.export_obj(vc_in_path)
         orig_width: float = context.object.dimensions[0]
         bpy.ops.object.delete(use_global=False)
         select_objects(objects, active_object)
-        self.setup_command(vc_in_path, [vc_out_path])
+        vc_out_paths = self.generate_output_paths(temp_dir)
+        self.setup_command(vc_in_path, vc_out_paths)
         print("Target Voxel Format:", self.vox_target_format_ext)
         success: bool = self.execute_voxconvert()
 
         if (success):
-            self.report({'INFO'}, f"{get_translation('info_generated_files')} {vc_out_path} ({get_file_size(vc_out_path)}) in {format_duration(self.voxconvert_duration)}")
+            for out_path in vc_out_paths:
+                self.report({'INFO'}, f"{get_translation('info_generated_files')} {out_path} ({get_file_size(out_path)}) in {format_duration(self.voxconvert_duration)}")
         else:
-            TempFileManager().delete_temp_dir(temp_dir)
             return {'CANCELLED'}
 
         start_time: float = time.time()
-        self.import_obj(vc_out_path)
+        obj_path: str = vc_out_paths[0]
+        self.import_obj(obj_path)
         self.set_scale(orig_width / context.object.dimensions[0])
         duration: str = format_duration(self.voxconvert_duration + (start_time - time.time()))
-        self.report({'INFO'}, f"{get_translation('info_vox_data_imported')} {vc_out_path} in {duration}")
+        self.report({'INFO'}, f"{get_translation('info_vox_data_imported')} {obj_path} in {duration}")
         self.report({'INFO'}, f"Voxelized in {format_duration(time.time() - voxelize_duration)}")
         properties = context.scene.voxility_pro_properties # FIXME: if type is specified as VoxilityProProperties it get circular error
-        if properties.voxformat_withcolor:
-            TempFileManager().delete_temp_dir(temp_dir)
 
-        #FIXME: we need to delete the temporary directory even without vertex colors but we can't because the color palette is used as texture in the imported object
-        #TempFileManager().delete_temp_dir(temp_dir)
         if properties.hide_original_objects:
             hide_objects_from_viewport(objects)
         return {'FINISHED'}
