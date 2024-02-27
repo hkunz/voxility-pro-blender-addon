11a16372a115f5d25372f9e2dff4ac3a12a71a38
Harry Kunz <harry.kunz@ymail.com>
2024-02-28 00:14:25 +0800

    WIP documentation

--- scripts/generate-documentation-content-html.sh
+++ scripts/generate-documentation-content-html.sh
@@ -11,6 +11,16 @@ generate_voxility_content_final_file() {
     cp "${DOCUMENT_CONTENT_TEMPLATE}" "${output_file}"
     sed -i -e '/nth-child/d' "$output_file"
 
+    replace_class_with_style_attribute $output_file
+    modify_alternate_row_colors $output_file
+
+    sed -i -e '/<style/,/<\/style>/d' -e '/<!--.*-->/d' -e '/^\s*$/d' "$output_file"
+    sed -i "1i $(get_autogenerate_notice_html)" "$output_file"
+    echo "Generated file: $output_file"
+}
+
+replace_class_with_style_attribute() {
+    local output_file="$1"
     CSS=$(sed -n '/<style>/,/<\/style>/p' "$output_file" | sed '1d;$d')
 
         while IFS= read -r line; do
@@ -19,24 +29,24 @@ generate_voxility_content_final_file() {
         echo Replace class=\"$class\" with style=\"$css\"
         sed -i "s/class=\"$class\"/style=\"$css\"/g" "$output_file"
     done <<< "$CSS"
+}
 
-    tr_tags=$(grep -o '<tr[^>]*style="[^"]*background-color:[^"]*"[^>]*>' "$output_file")
- 
-    awk 'BEGIN { alternate = 0; } \
-        /<tr[^>]*>/ { \
-            if (alternate == 0) { \
-                gsub(/background-color:[^;]+;/, "background-color: #dfdfdf;", $0); \
-                alternate = 1; \
-            } else { \
-                gsub(/background-color:[^;]+;/, "background-color: #f1f1f1;", $0); \
-                alternate = 0; \
-            } \
-        } \
-        { print }' "$output_file" > temp.html && mv temp.html "$output_file"
+modify_alternate_row_colors() {
+    local output_file="$1"
+    local i=0
 
-    sed -i -e '/<style/,/<\/style>/d' -e '/<!--.*-->/d' -e '/^\s*$/d' "$output_file"
-    sed -i "1i $(get_autogenerate_notice_html)" "$output_file"
-    echo "Generated file: $output_file"
+    tr_tags=$(grep -o '<tr[^>]*style="[^"]*background-color:[^"]*background-color:[^"]*"[^>]*>.*' "$output_file")
+    color=""
+    while IFS= read -r tr_tag; do
+        if (( i % 2 == 0 )); then
+            color=$(echo "$tr_tag" | grep -o 'background-color:[^;]*; ' | head -n 1)
+        else
+            color=$(echo "$tr_tag" | grep -o 'background-color:[^;]*;' | sed '2!d')
+        fi
+        modified_line=$(echo "$tr_tag" | sed "s/${color}//")
+        sed -i "s|$tr_tag|$modified_line|" "$output_file"
+        ((i++))
+    done <<< "$tr_tags"
 }
 
 generate_voxility_content_final_file
