ae360a8008385d94962ab23325005c91bb8c9f43
Harry Kunz <harry.kunz@ymail.com>
2024-03-26 14:00:17 +0800

    wip continue validity test/checks for voxelizing object export

--- operators/voxel/operator_voxelize_validity_check.py
+++ operators/voxel/operator_voxelize_validity_check.py
@@ -12,46 +12,59 @@ class VoxelError:
     ERROR_MULTIPLE_MATERIAL_OUTPUT_NODES = 2
     ERROR_MISSING_INPUT_IN_MATERIAL_OUTPUT_NODE = 3
     ERROR_ONLY_BSDF_SUPPORTED_IN_MATERIAL_OUTPUT_NODE = 4
-    ERROR_ONLY_TEXTURE_OR_COLOR_ATTR_SUPPORTED_IN_BSDF_COLOR_INPUT = 5
-    ERROR_MISSING_MATERIAL_OUTPUT_NODE = 6
-    ERROR_MISSING_IMAGE_TEXTURE_IMAGE = 7
-    ERROR_OBJECT_MISSING_UVMAPS = 8
-    ERROR_MISSING_UVMAP_NODE_INPUT_TO_IMAGE_TEXTURE = 9
-    ERROR_INVALID_UVMAP_NAME_USED_IN_VOXILITY_PANEL = 10
-    ERROR_INVALID_UVMAP_NAME_USED_IN_UVMAP_NODE = 11
-    ERROR_UVMAP_NODE_INPUT_REQUIRED_TO_IMAGE_TEXTURE = 12
-    WARNING_NO_MATERIALS = 13
+    ERROR_SURFACE_SOCKET_SUPPORTED_IN_MATERIAL_OUTPUT_NODE = 5
+    ERROR_ONLY_TEXTURE_OR_COLOR_ATTR_SUPPORTED_IN_BSDF_COLOR_INPUT = 6
+    ERROR_MISSING_MATERIAL_OUTPUT_NODE = 7
+    ERROR_MISSING_IMAGE_TEXTURE_IMAGE = 8
+    ERROR_MUST_USE_IMAGE_TEXTURE_COLOR_SOCKET = 9
+    ERROR_OBJECT_MISSING_UVMAPS = 10
+    ERROR_MISSING_UVMAP_NODE_INPUT_TO_IMAGE_TEXTURE = 11
+    ERROR_INVALID_UVMAP_NAME_USED_IN_VOXILITY_PANEL = 12
+    ERROR_INVALID_UVMAP_NAME_USED_IN_UVMAP_NODE = 13
+    ERROR_ONLY_ATTRIBUTE_NODE_GEOMETRY_TYPE_ALLOWED_FOR_UVMAP_USE = 14
+    ERROR_ONLY_ATTRIBUTE_NODE_VECTOR_SOCKET_ALLOWED_FOR_UVMAP_USE = 15
+    ERROR_INVALID_ATTRIBUTE_NODE_OUTPUT_SOCKET_USED_FOR_UVMAP = 16
+    ERROR_UVMAP_NODE_INPUT_REQUIRED_TO_IMAGE_TEXTURE = 17
+    WARNING_NO_MATERIALS = 18
 
     @staticmethod
     def get_error(e):
         if e == VoxelError.ERROR_NONE:
             return "No errors"
         if e == VoxelError.ERROR_MULTIPLE_BSDF_NODES:
-            return "Only one \"Principled BSDF\" node allowed"
+            return "Only one 'Principled BSDF' node allowed"
         if e == VoxelError.ERROR_MULTIPLE_MATERIAL_OUTPUT_NODES:
-            return "Only one \"Material Output\" node allowed"
+            return "Only one 'Material Output' node allowed"
         if e == VoxelError.ERROR_MISSING_INPUT_IN_MATERIAL_OUTPUT_NODE:
-            return "Missing input to \"Material Output\" node's \"Surface\" socket"
+            return "Missing input to 'Surface' socket of 'Material Output' node"
+        if e == VoxelError.ERROR_SURFACE_SOCKET_SUPPORTED_IN_MATERIAL_OUTPUT_NODE:
+            return "Only 'Surface' socket of 'Material Output' node supported"
         if e == VoxelError.ERROR_ONLY_BSDF_SUPPORTED_IN_MATERIAL_OUTPUT_NODE:
-            return "Only \"Princpled BSDF\" allowed in \"Material Output\" node"
+            return "Only 'Princpled BSDF' allowed to 'Surface' socket of 'Material Output' node"
         if e == VoxelError.ERROR_ONLY_TEXTURE_OR_COLOR_ATTR_SUPPORTED_IN_BSDF_COLOR_INPUT:
-            return "Only \"Image Texture\" or \"Color Attribute\" nodes allowed in \"Principled BSDF\" node's \"Base Color\" socket" # 91 characters max equivalent to 520 width
+            return "Only 'Image Texture' or 'Color Attribute' nodes allowed to 'Base Color' socket of 'Principled BSDF' node" # 91 characters max equivalent to 520 width
         if e == VoxelError.ERROR_MISSING_MATERIAL_OUTPUT_NODE:
-            return "Missing \"Material Output\" node"
+            return "Missing 'Material Output' node"
         if e == VoxelError.ERROR_MISSING_IMAGE_TEXTURE_IMAGE:
-            return "Missing image in \"Image Texture\""
+            return "Missing image in 'Image Texture'"
+        if e == VoxelError.ERROR_MUST_USE_IMAGE_TEXTURE_COLOR_SOCKET:
+            return "Only 'Color' socket of 'Image Texture' node allowed to 'Base Color' socket of 'Principled BSDF'"
         if e == VoxelError.ERROR_OBJECT_MISSING_UVMAPS:
-            return "Object \"PARAM\" has no UV Maps"
+            return "Object 'PARAM' has no UV Maps"
         if e == VoxelError.ERROR_MISSING_UVMAP_NODE_INPUT_TO_IMAGE_TEXTURE:
-            return "Missing \"UV Map\" node in \"Image Texture\" node's \"Vector\" socket"
+            return "Missing 'UV Map' node to 'Vector' socket of 'Image Texture' node"
         if e == VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_VOXILITY_PANEL:
             return "Invalid UV Map name specified in Voxility panel. Possible UV Map values are: PARAM"
         if e == VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_UVMAP_NODE:
-            return "Invalid UV Map name used in \"UV Map\" node. You must use name specified in Voxility panel which is \"PARAM\""
+            return "Invalid UV Map name used in 'UV Map' node. You must use name specified in Voxility panel which is 'PARAM'"
+        if e == VoxelError.ERROR_ONLY_ATTRIBUTE_NODE_GEOMETRY_TYPE_ALLOWED_FOR_UVMAP_USE:
+            return "Invalid attribute type 'PARAM' used for UV Map. You must use 'GEOMETRY' for 'Attribute' node used as UV Map"
+        if e == VoxelError.ERROR_ONLY_ATTRIBUTE_NODE_VECTOR_SOCKET_ALLOWED_FOR_UVMAP_USE:
+            return "Only 'Vector' socket of 'Attribute' node allowed for UV Map"
         if e == VoxelError.ERROR_UVMAP_NODE_INPUT_REQUIRED_TO_IMAGE_TEXTURE:
-            return "A \"UV Map\" node is required as input to \"Image Texture\" node's \"Vector\" socket"
+            return "A 'UV Map' node is required as input to 'Vector' socket of 'Image Texture' node"
         if e == VoxelError.WARNING_NO_MATERIALS:
-            return "No materials found (or \"Use Nodes\" is disabled)"
+            return "No materials found (or 'Use Nodes' is disabled)"
         return "Undefined Error"
 
 class OBJECT_OT_OperatorVoxelizeValidityCheck(OperatorGenericPopup):
@@ -105,7 +118,6 @@ class OBJECT_OT_OperatorVoxelizeValidityCheck(OperatorGenericPopup):
     def get_errors(self, context):
         errors = []
         for obj in context.selected_objects:
-            #print(f"Check for problems in materials in object \"{obj.name}\":")
             num_mat = 0
             for slot in obj.material_slots:
                 mat = slot.material
@@ -120,6 +132,10 @@ class OBJECT_OT_OperatorVoxelizeValidityCheck(OperatorGenericPopup):
                         self.add_error(mat, errors, VoxelError.ERROR_MULTIPLE_MATERIAL_OUTPUT_NODES)
                     matout_node = n
                     from_node = self.get_from_input_node(matout_node)
+                    input_volume = self.get_from_input_node(matout_node, 1)
+                    input_displ = self.get_from_input_node(matout_node, 2)
+                    if input_volume or input_displ:
+                        self.add_error(mat, errors, VoxelError.ERROR_SURFACE_SOCKET_SUPPORTED_IN_MATERIAL_OUTPUT_NODE)
                     if not from_node:
                         self.add_error(mat, errors, VoxelError.ERROR_MISSING_INPUT_IN_MATERIAL_OUTPUT_NODE)
                     elif from_node.type != "BSDF_PRINCIPLED":
@@ -127,28 +143,41 @@ class OBJECT_OT_OperatorVoxelizeValidityCheck(OperatorGenericPopup):
                     else:
                         bsdf_node = from_node
                         from_node = self.get_from_input_node(bsdf_node)
+                        from_socket = self.get_from_input_socket(bsdf_node)
                         _, vox_uvmap, vox_colattr = get_voxelizer_voxel_modifier_attributes(obj)
                         if not from_node:
                             pass # pass coz it's ok to use base color or direct color from Principled BSDF
                         elif from_node.type == 'TEX_IMAGE':
                             tex_node = from_node
-                            if not obj.data.uv_layers.keys():
+                            if from_socket.type != 'RGBA':
+                                self.add_error(mat, errors, VoxelError.ERROR_MUST_USE_IMAGE_TEXTURE_COLOR_SOCKET)
+                            uvmaps = obj.data.uv_layers.keys()
+                            if not uvmaps:
                                 self.add_error(mat, errors, VoxelError.ERROR_OBJECT_MISSING_UVMAPS, obj.name)
-                            elif not tex_node.image:
+                            elif vox_uvmap not in obj.data.uv_layers:
+                                    self.add_error(mat, errors, VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_VOXILITY_PANEL, str(uvmaps))
+                            if not tex_node.image:
                                 self.add_error(mat, errors, VoxelError.ERROR_MISSING_IMAGE_TEXTURE_IMAGE)
-                            else:
-                                #TODO check if Color output socket is used instead of Alpha
-                                from_node = self.get_from_input_node(tex_node)
-                                if not from_node:
-                                    self.add_error(mat, errors, VoxelError.ERROR_MISSING_UVMAP_NODE_INPUT_TO_IMAGE_TEXTURE)
-                                elif from_node.type != 'UVMAP':
-                                    self.add_error(mat, errors, VoxelError.ERROR_UVMAP_NODE_INPUT_REQUIRED_TO_IMAGE_TEXTURE)
-                                else:
-                                    uvmap_node = from_node
-                                    if vox_uvmap not in obj.data.uv_layers:
-                                        self.add_error(mat, errors, VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_VOXILITY_PANEL, str(obj.data.uv_layers.keys()))
-                                    elif not uvmap_node.uv_map or uvmap_node.uv_map != vox_uvmap:
-                                        self.add_error(mat, errors, VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_UVMAP_NODE, vox_uvmap)
+                            from_node = self.get_from_input_node(tex_node)
+                            if not from_node:
+                                self.add_error(mat, errors, VoxelError.ERROR_MISSING_UVMAP_NODE_INPUT_TO_IMAGE_TEXTURE)
+                            elif from_node.type != 'UVMAP' and from_node.type != 'ATTRIBUTE':
+                                self.add_error(mat, errors, VoxelError.ERROR_UVMAP_NODE_INPUT_REQUIRED_TO_IMAGE_TEXTURE)
+                            elif from_node.type == 'UVMAP':
+                                uvmap_node = from_node
+                                if uvmaps and (not uvmap_node.uv_map or uvmap_node.uv_map != vox_uvmap):
+                                    self.add_error(mat, errors, VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_UVMAP_NODE, vox_uvmap)
+                            elif from_node.type == 'ATTRIBUTE':
+                                attr_node = from_node
+                                from_socket = self.get_from_input_socket(tex_node)
+                                if from_socket and from_socket.type != 'VECTOR':
+                                    self.add_error(mat, errors, VoxelError.ERROR_ONLY_ATTRIBUTE_NODE_VECTOR_SOCKET_ALLOWED_FOR_UVMAP_USE)
+                                if attr_node.attribute_type != 'GEOMETRY':
+                                    self.add_error(mat, errors, VoxelError.ERROR_ONLY_ATTRIBUTE_NODE_GEOMETRY_TYPE_ALLOWED_FOR_UVMAP_USE, attr_node.attribute_type)
+                                if uvmaps and (not attr_node.attribute_name or attr_node.attribute_name != vox_uvmap):
+                                    self.add_error(mat, errors, VoxelError.ERROR_INVALID_UVMAP_NAME_USED_IN_UVMAP_NODE, vox_uvmap)
+                                    
+                                    #['Image Texture'].inputs[0].links[0].from_socket.type
 
                         elif from_node.type == 'VERTEX_COLOR':
                             #bpy.data.scenes["Scene"].voxility_pro_properties.color_attribute
@@ -175,13 +204,15 @@ class OBJECT_OT_OperatorVoxelizeValidityCheck(OperatorGenericPopup):
             return True
         return False
 
-    def get_from_input_node(self, node):
-        links = node.inputs[0].links
+    def get_from_input_node(self, node, index=0):
+        links = node.inputs[index].links
         return links[0].from_node if len(links) > 0 else None
 
-    def get_to_output_node(self, node):
-        links = node.outputs[0].links
-        return links[0].to_node if len(links) > 0 else None
+    def get_from_input_socket(self, node, index=0):
+        links = node.inputs[index].links
+        return links[0].from_socket if len(links) > 0 else None
+
+
 
     @classmethod
     def poll(cls, context):
