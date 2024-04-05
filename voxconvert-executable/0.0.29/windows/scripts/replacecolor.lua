ea10ce354cf3a566390193508f6b1ffe00eb9104
Harry Kunz <harry.kunz@ymail.com>
2024-04-05 21:49:05 +0800

    add latest voxconvert executables for windows/mac/linux

---
+++ voxconvert-executable/0.0.29/windows/scripts/replacecolor.lua
@@ -0,0 +1,27 @@
+--
+-- replace one palette color with another one
+--
+
+local vol = require "modules.volume"
+
+function arguments()
+	return {
+		{ name = 'newcolor', desc = 'the palette color index', type = 'colorindex' }
+	}
+end
+
+function main(node, region, color, newcolor)
+	local volume = node:volume()
+	local visitor = function (volume, x, y, z)
+		volume:setVoxel(x, y, z, newcolor)
+	end
+
+	local condition = function (volume, x, y, z)
+		local voxel = volume:voxel(x, y, z)
+		if voxel == color then
+			return true
+		end
+		return false
+	end
+	vol.conditionYXZ(volume, region, visitor, condition)
+end
